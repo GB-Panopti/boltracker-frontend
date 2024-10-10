80a2e3baa9088dc77186cf480cb6c789929793f2
GregorFC <g.e.figueiracomojo@student.tudelft.nl>
2024-10-10 14:14:29 +0200

    Fix mobile track product button

--- src/components/ui/navigation/MobileSidebar.tsx
+++ src/components/ui/navigation/MobileSidebar.tsx
@@ -106,25 +106,11 @@ export default function MobileSidebar() {
                 </li>
 
                 <li key="Track product">
-                  <Link
-                    href="#"
-                    className={cx(
-                      "flex items-center gap-x-2.5 rounded-md px-2 py-1.5 text-md font-medium transition hover:dark:bg-gray-900 bg-gb-primarylite-600 text-gb-primarylite-50 ring-gb-primarylite-600/30 dark:bg-gb-primarylite-400/20 dark:text-gb-primarylite-400 dark:ring-gb-primarylite-400/20 hover:bg-gb-secondary-400 ",
-                      // isActive(siteConfig.baseLinks.trackNew) ? "text-gb-secondary-600 dark:text-gb-secondary-400" : "",
-
-                      focusRing
-                    )}
-                  >
-                    <RiRadarLine
-                      className="size-5 shrink-0"
-                      aria-hidden="true"
-                    />
                     <ModalAddProduct
                       onSelect={handleDialogItemSelect}
                       onOpenChange={handleDialogItemOpenChange}
                       itemName="Track product"
                     />
-                  </Link>
                 </li>
               </ul>
               <div>
