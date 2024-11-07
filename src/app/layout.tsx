5f03ed8986392bae026e3d01ab91567e7cb387af
GregorFC <g.e.figueiracomojo@student.tudelft.nl>
2024-11-07 14:04:16 +0100

    Microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,6 +79,14 @@ export default function RootLayout({
           gtag('config', 'AW-16750157391');
         `}
       </Script>
+
+      <Script id="clarity-script" strategy="beforeInteractive">
+            {`(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ottbrmrram");`}
+        </Script>
         {children}
       </body>
     </html>
